5b3c8aff92a4707ff7f6366dcf753b4026fa9e7f
Charles Fulnecky <charles.fulnecky@hbo.com>
2015-01-24 18:16:14 -0500

    Rolling back for now.

--- cmelion-routeprovider.src.js
+++ cmelion-routeprovider.src.js
@@ -1,52 +1,49 @@
 (function(){
-  "use strict";
+    "use strict";
 
-  angular.module('cmelion.routeProvider', [])
+    angular.module('cmelion.routeProvider', [])
         .constant('stateFactory', stateFactory);
 
-        stateFactory.$inject = ['classedName', 'params'];
-        function stateFactory(classedName, params) {
+    function stateFactory(classedName, params) {
 
-            var _INITSERVICE = classedName + 'CtrlInit';
+        var _INITSERVICE = classedName + 'CtrlInit';
 
-        function dasherize(classedName) {
-            return classedName
-                    .replace(/(?:^[A-Z]{2,})/g, function (match) { //XMLfileIsCool -> xml-fileIsCool
-                        return match.toLowerCase() + "-";
-                    })
-                    .replace(/(?:[A-Z]+)/g, function (match) { //camelCase -> snake-case
-                        return "-" + match.toLowerCase();
-                    })
-                    .replace(/^-/, ''); // CamelCase -> -snake-case -> snake-case
-            }
-
-
-        var dasherizedName = dasherize(classedName);
+        function dasherize(input) {
+            return input
+                .replace(/(?:^[A-Z]{2,})/g, function (match) { //XMLfileIsCool -> xml-fileIsCool
+                    return match.toLowerCase() + "-";
+                })
+                .replace(/(?:[A-Z]+)/g, function (match) { //camelCase -> snake-case
+                    return "-" + match.toLowerCase();
+                })
+                .replace(/^-/, ''); // CamelCase -> -snake-case -> snake-case
+        }
 
         var _defaults = {
-            url: '/' + dasherizedName,
+            url: '/' + dasherize(classedName),
+            templateUrl: 'states/' + dasherize(classedName) + '/index/main-view.html',
             controller: classedName + 'Ctrl',
             controllerAs: 'vm'
         };
 
-            try {
-                _defaults.resolve = {
-                    init: ['$injector', function ($injector) {
-                        if ($injector.has(_INITSERVICE)) {
-                            var service = $injector.get(_INITSERVICE);
-                            if (typeof service.prepare !== 'function') throw _INITSERVICE + ' has no prepare method.';
+        try {
+            _defaults.resolve = {
+                init: ['$injector', function ($injector) {
+                    if ($injector.has(_INITSERVICE)) {
+                        var service = $injector.get(_INITSERVICE);
+                        if (typeof service.prepare !== 'function') throw _INITSERVICE + ' has no prepare method.';
 
-                            return service.prepare();
-                        }
-                    }]
-                }
+                        return service.prepare();
+                    }
+                }]
             }
-            catch (e) {
-                throw 'Serious error occurred trying to load controller.: ' + e;
-            }
-
-            return angular.extend(_defaults, params);
-
         }
+        catch (e) {
+            throw 'Serious error occurred trying to load controller.: ' + e;
+        }
+
+        return angular.extend(_defaults, params);
+
+    }
 
 }());
